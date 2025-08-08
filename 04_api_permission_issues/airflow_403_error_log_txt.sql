516358fd5e097e3be561f5ffbce2322382edfe3d
Apoorva <156057807+Apoorva-888@users.noreply.github.com>
2025-08-08 21:04:03 +0530

    Create airflow_403_error_log_txt.sql

---
+++ 04_api_permission_issues/airflow_403_error_log_txt.sql
@@ -0,0 +1,5 @@
+[2025-07-28, 11:00:20] ERROR - Task failed with exception
+HTTPError: 403 Client Error: Forbidden for url: https://api.example.com/protected-resource
+File "/usr/local/lib/python3.12/site-packages/requests/models.py", line 1026, in raise_for_status
+raise HTTPError(http_error_msg, response=self)
+requests.exceptions.HTTPError: 403 Client Error: Forbidden for url
